e0eddc67916485ab090cdc0afe096e9719f3fb13
Mudit0704 <maheshwari7mudit@gmail.com>
2019-07-25 23:56:42 +0530

    table added

--- viewallbooks.php
+++ viewallbooks.php
@@ -16,30 +16,85 @@
       $qr1 = "select * from books_details where bcid = ".$row[0]."";
       $res1 = mysqli_query($cn, $qr1);
       ?>
-      <table align="center" border="1">
-  			<tr>
-  				<th>Book ID</th>
-  				<th>Book Name</th>
-          <th>Condition</th>
-          <th>isAvailable</th>
-  			</tr>
+      <div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
 
+            <div class="panel panel-default panel-table">
+              <div class="panel-heading">
+                <div class="row">
+                  <div class="col col-xs-6">
+                    <h3 class="panel-title">All Books</h3>
+                  </div>
+                  <div class="col col-xs-6 text-right">
+                    <a href="lenderhomepg.php" type="button" class="btn btn-sm btn-primary btn-create">Go Back</a>
+                  </div>
+                </div>
+              </div>
+              <div class="panel-body">
+                <table class="table table-striped table-bordered table-list">
+                  <thead>
+                    <tr>
+                        <th><em class="fa fa-cog"></em></th>
+                        <th class="hidden-xs">Book ID</th>
+                        <th>Book Name</th>
+                        <th>Condition</th>
+                        <th>isAvailable</th>
+                    </tr> 
+                  </thead>
   			<?php
   				while($row1 = mysqli_fetch_array($res1))
   				{
 
   			?>
-  			<tr>
-  				<td><?php echo $row1[0] ?></td>
-  				<td><?php echo $row1[1] ?></td>
-          <td><?php echo $row1[8] ?></td>
-          <td><?php echo $row1[9] ?></td>
-        </tr>
+          <tbody>
+                          <tr>
+                            <td align="center">
+                              <a class="btn btn-default"><em class="fa fa-pencil"></em></a>
+                              <a class="btn btn-danger"><em class="fa fa-trash"></em></a>
+                            </td>
+                            <td class="hidden-xs"><?php echo $row1[0] ?></td>
+                            <td><?php echo $row1[1] ?></td>
+                            <td><?php echo $row1[8] ?></td>
+                            <td><?php echo $row1[9] ?></td>
+                          </tr>
+                        </tbody>
+                </table>
+
         <?php
           }
          ?>
-       </table>
-       <center><h3>Press <a href="lenderhomepg.php">here</a> to go to home page!</h3></center>
+       
+
+
+
+              
+            
+              </div>
+              <div class="panel-footer">
+                <div class="row">
+                  <div class="col col-xs-4">Page 1 of 5
+                  </div>
+                  <div class="col col-xs-8">
+                    <ul class="pagination hidden-xs pull-right">
+                      <li><a href="#">1</a></li>
+                      <li><a href="#">2</a></li>
+                      <li><a href="#">3</a></li>
+                      <li><a href="#">4</a></li>
+                      <li><a href="#">5</a></li>
+                    </ul>
+                    <ul class="pagination visible-xs pull-right">
+                        <li><a href="#">«</a></li>
+                        <li><a href="#">»</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+</div>
+</div>
+</div>
 	</body>
 
 
