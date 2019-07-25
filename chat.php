fa508c6c2ce7a91fb0d08f77ae3a767ac1878e65
Mudit0704 <maheshwari7mudit@gmail.com>
2019-07-26 01:17:00 +0530

    Chat

--- chat.php
+++ chat.php
@@ -27,28 +27,43 @@ textarea:focus {
           $qr = "select * from chat";
           $res = mysqli_query($cn, $qr);
     ?>
-    <table align="center" border="1">
-			<tr>
-        <th>Msg ID</th>
-				<th>User ID</th>
-        <th>User Name</th>
-				<th>User Type</th>
-        <th>Message</th>
+    	<div class="container">
+    <div class="row">
+    
+        <div class="col-md-10 col-md-offset-1">
 
-			</tr>
-
+            <div class="panel panel-default panel-table">
+              <div class="panel-heading">
+                <div class="row">
+                  <div class="col col-xs-6">
+                    <h3 class="panel-title">Panel Heading</h3>
+                  </div>
+                  <div class="col col-xs-6 text-right">
+                    <button type="button" class="btn btn-sm btn-primary btn-create">Create New</button>
+                  </div>
+                </div>
+              </div>
+              <div class="panel-body">
+                <table class="table table-striped table-borderless table-info">
+                  <thead>
+                    <tr>
+                        <th class="hidden-xs">User Name</th>
+						<th>User Type</th>
+				        <th>Message</th>
+                    </tr> 
+                  </thead>
+                  <tbody>
 			<?php
 				while($row = mysqli_fetch_array($res))
 				{
 
 			?>
-			<tr>
-				<td><?php echo $row[0] ?></td>
-				<td><?php echo $row[1] ?></td>
-        <td><?php echo $row[2] ?></td>
-        <td><?php echo $row[3] ?></td>
-        <td><?php echo $row[4] ?></td>
-      </tr>
+				<tr>
+                           <td><?php echo $row[2] ?></td>
+					        <td><?php echo $row[3] ?></td>
+					        <td><?php echo $row[4] ?></td>
+                          </tr>
+
       <?php
         }
        ?>
@@ -69,6 +84,19 @@ textarea:focus {
 		?>
 
 		<center> <?php echo $msg ?> </center>
+
+
+		
+                          
+                        </tbody>
+                </table>
+            
+              </div>
+            </div>
+
+</div></div></div>
+
+
 	</body>
 
 
