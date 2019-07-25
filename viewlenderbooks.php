6b3b52fe764d1f35123792c5ec5e1e89b67b2371
Mudit0704 <maheshwari7mudit@gmail.com>
2019-07-26 00:15:05 +0530

    Your books added

--- viewlenderbooks.php
+++ viewlenderbooks.php
@@ -37,33 +37,63 @@
         $qr1 = "select * from books_details where boid = ".$_SESSION["lid"]."";
         $res1 = mysqli_query($cn, $qr1);
       ?>
-      <table align="center" border="1">
-  			<tr>
-  				<th>Book ID</th>
-  				<th>Book Name</th>
-          <th>Condition</th>
-          <th>isAvailable</th>
-          <th>Action</th>
-  			</tr>
+      <div class="container">
+    <div class="row">
+
+        <div class="col-md-10 col-md-offset-1">
+
+            <div class="panel panel-default panel-table">
+              <div class="panel-heading">
+                <div class="row">
+                  <div class="col col-xs-6">
+                    <h3 class="panel-title">Panel Heading</h3>
+                  </div>
+                  <div class="col col-xs-6 text-right">
+                    <a type="button" href="lenderhomepg.php" class="btn btn-sm btn-primary btn-create">Go Back</a>
+                  </div>
+                </div>
+              </div>
+              <div class="panel-body">
+                <table class="table table-striped table-bordered table-list">
+                  <thead>
+                    <tr>
+                        <th><em class="fa fa-cog"></em></th>
+                        <th class="hidden-xs"> Book ID</th>
+                        <th>Book Name</th>
+                        <th>Condition</th>
+                        <th>isAvailable</th>
+                        <th>Action</th>
+                    </tr> 
+                  </thead>
+                  <tbody>
 
   			<?php
   				while($row1 = mysqli_fetch_array($res1))
   				{
 
   			?>
-  			<tr>
-  				<td><?php echo $row1[0] ?></td>
-  				<td><?php echo $row1[1] ?></td>
-          <td><?php echo $row1[8] ?></td>
-          <td><?php echo $row1[9] ?></td>
-          <td><a href="brand_update.php?brand_id=<?php echo $row[0] ?>">Edit </a> <a href="javascript:del(<?php echo $row[0] ?>)">Delete</a></td>
-        </tr>
+  			
+                          <tr>
+                            <td align="center">
+                              <a href="brand_update.php?brand_id=<?php echo $row[0] ?>" class="btn btn-default"><em class="fa fa-pencil"></em></a>
+                              <a href="javascript:del(<?php echo $row[0] ?>)" class="btn btn-danger"><em class="fa fa-trash"></em></a>
+                            </td>
+                            <td class="hidden-xs"><?php echo $row1[0] ?></td>
+                            <td><?php echo $row1[1] ?></td>
+                            <td><?php echo $row1[8] ?></td>
+                            <td><?php echo $row1[9] ?></td>
+                          </tr>
+                       
         <?php
           }
         }
          ?>
+          </tbody>
        </table>
-       <center><h3>Press <a href="lenderhomepg.php">here</a> to go to home page!</h3></center>
+              </div>
+            </div>
+
+</div></div></div>
 	</body>
 
 
