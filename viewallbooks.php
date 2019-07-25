dadd5c87e656fdbcf64acb0fb84c00f512884d8f
Mudit0704 <maheshwari7mudit@gmail.com>
2019-07-26 00:00:50 +0530

    table modified

--- viewallbooks.php
+++ viewallbooks.php
@@ -35,7 +35,6 @@
                 <table class="table table-striped table-bordered table-list">
                   <thead>
                     <tr>
-                        <th><em class="fa fa-cog"></em></th>
                         <th class="hidden-xs">Book ID</th>
                         <th>Book Name</th>
                         <th>Condition</th>
@@ -49,47 +48,17 @@
   			?>
           <tbody>
                           <tr>
-                            <td align="center">
-                              <a class="btn btn-default"><em class="fa fa-pencil"></em></a>
-                              <a class="btn btn-danger"><em class="fa fa-trash"></em></a>
-                            </td>
                             <td class="hidden-xs"><?php echo $row1[0] ?></td>
                             <td><?php echo $row1[1] ?></td>
                             <td><?php echo $row1[8] ?></td>
                             <td><?php echo $row1[9] ?></td>
                           </tr>
                         </tbody>
-                </table>
-
         <?php
           }
          ?>
-       
-
-
-
-              
-            
-              </div>
-              <div class="panel-footer">
-                <div class="row">
-                  <div class="col col-xs-4">Page 1 of 5
-                  </div>
-                  <div class="col col-xs-8">
-                    <ul class="pagination hidden-xs pull-right">
-                      <li><a href="#">1</a></li>
-                      <li><a href="#">2</a></li>
-                      <li><a href="#">3</a></li>
-                      <li><a href="#">4</a></li>
-                      <li><a href="#">5</a></li>
-                    </ul>
-                    <ul class="pagination visible-xs pull-right">
-                        <li><a href="#">«</a></li>
-                        <li><a href="#">»</a></li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
+         </table>
+       </div>
             </div>
 
 </div>
