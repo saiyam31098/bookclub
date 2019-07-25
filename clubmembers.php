9993bde043ecbe062a1532b54c64d4cc2520b13e
Mudit0704 <maheshwari7mudit@gmail.com>
2019-07-26 00:36:27 +0530

    bug fixes and clubmember added

--- clubmembers.php
+++ clubmembers.php
@@ -13,22 +13,46 @@
           $qr = "select * from lender_details where lcid = ".$_SESSION["lcid"]."";
           $res = mysqli_query($cn, $qr);
     ?>
-    <table align="center" border="1">
-			<tr>
-				<th>Member ID</th>
-				<th>Member Name</th>
-        <th>Member Email</th>
-			</tr>
+   
+      <div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+
+            <div class="panel panel-default panel-table">
+              <div class="panel-heading">
+                <div class="row">
+                  <div class="col col-xs-6">
+                    <h3 class="panel-title">Panel Heading</h3>
+                  </div>
+                  <div class="col col-xs-6 text-right">
+                    <a href="lenderhomepg.php" type="button" class="btn btn-sm btn-primary btn-create">Go Back</a>
+                  </div>
+                </div>
+              </div>
+              <div class="panel-body">
+                <table class="table table-striped table-bordered table-list">
+                  <thead>
+                    <tr>
+                        <th class="hidden-xs">Member ID</th>
+                        <th>Member Name</th>
+                        <th>Member Email</th>
+                    </tr> 
+                  </thead>
+                   <tbody>
 
 			<?php
 				while($row = mysqli_fetch_array($res))
 				{
 
 			?>
-			<tr>
-				<td><?php echo $row[0] ?></td>
-				<td><?php echo $row[1] ?></td>
-        <td><?php echo $row[5] ?></td>
+			 <tr>
+                            <td align="center">
+                              <a class="btn btn-default"><em class="fa fa-pencil"></em></a>
+                              <a class="btn btn-danger"><em class="fa fa-trash"></em></a>
+                            </td>
+                            <td class="hidden-xs"><?php echo $row[0] ?></td>
+                            <td><?php echo $row[1] ?></td>
+                            <td><?php echo $row[5] ?></td>
       </tr>
       <?php
         }
@@ -37,7 +61,16 @@
      <?php
       }
       ?>
-      <center><h3>Press <a href="lenderhomepg.php">here</a> to go to home page!</h3></center>
+
+                 
+                         
+                        </tbody>
+                </table>
+              </div>
+            </div>
+
+</div></div></div>
+
 	</body>
 
 
